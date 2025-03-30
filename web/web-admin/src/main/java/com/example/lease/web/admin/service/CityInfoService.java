2913cde5502b6b6c66d8283d8ee5f81a5438628b
Wa_Automata <550578884@qq.com>
2025-03-31 01:02:26 +0800

    地区信息管理

--- web/web-admin/src/main/java/com/example/lease/web/admin/service/CityInfoService.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/CityInfoService.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
 * @author cly
 * @description 针对表【city_info】的数据库操作Service
-* @createDate 2025--03-24 15:48:00
+* @createDate 2025-03-24 15:48:00
 */
 public interface CityInfoService extends IService<CityInfo> {
 
--- web/web-admin/src/main/java/com/example/lease/web/admin/service/ProvinceInfoService.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/ProvinceInfoService.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
 * @author cly
 * @description 针对表【province_info】的数据库操作Service
-* @createDate 2025--03-24 15:48:00
+* @createDate 2025-03-24 15:48:00
 */
 public interface ProvinceInfoService extends IService<ProvinceInfo> {
 
