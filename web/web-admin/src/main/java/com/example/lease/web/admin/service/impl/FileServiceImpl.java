7268ec148888939d1f3bd0a38d72aafd07265cdf
Wa_Automata <550578884@qq.com>
2025-04-16 00:38:16 +0800

    图片上传管理

--- web/web-admin/src/main/java/com/example/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/impl/FileServiceImpl.java
@@ -3,10 +3,14 @@ package com.example.lease.web.admin.service.impl;
 import com.example.lease.common.minio.MinioProperties;
 import com.example.lease.web.admin.service.FileService;
 import io.minio.*;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -18,44 +22,37 @@ public class FileServiceImpl implements FileService {
     private MinioProperties properties;
 
     @Autowired
-    private MinioClient client;
+    private MinioClient minioClient;
 
     @Override
-    public String upload(MultipartFile file) {
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
-        try {
-            boolean bucketExists = client.bucketExists(
-                    BucketExistsArgs.builder()
+        boolean bucketExists = minioClient.bucketExists(
+                BucketExistsArgs.builder()
+                        .bucket(properties.getBucketName())
+                        .build());
+        if (!bucketExists) {
+            minioClient.makeBucket(
+                    MakeBucketArgs.builder()
                             .bucket(properties.getBucketName())
                             .build());
-            if (!bucketExists) {
-                client.makeBucket(
-                        MakeBucketArgs.builder()
-                                .bucket(properties.getBucketName())
-                                .build());
-                client.setBucketPolicy(
-                        SetBucketPolicyArgs.builder()
-                                .bucket(properties.getBucketName())
-                                .config(createBucketPolicyConfig(properties.getBucketName()))
-                                .build());
-            }
-
-            String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) +
-                    "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
-            client.putObject(
-                    PutObjectArgs.builder()
+            minioClient.setBucketPolicy(
+                    SetBucketPolicyArgs.builder()
                             .bucket(properties.getBucketName())
-                            .stream(file.getInputStream(), file.getSize(), -1)
-                            .object(filename)
-//                            .contentType(file.getContentType())
+                            .config(createBucketPolicyConfig(properties.getBucketName()))
                             .build());
-
-            return String.join("/", properties.getEndpoint(), properties.getBucketName(), filename);
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
+        String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) +
+                "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(properties.getBucketName())
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .object(filename)
+                        .contentType(file.getContentType())
+                        .build());
+
+        return String.join("/",properties.getEndpoint(),properties.getBucketName(),filename);
     }
 
     private String createBucketPolicyConfig(String bucketName) {
