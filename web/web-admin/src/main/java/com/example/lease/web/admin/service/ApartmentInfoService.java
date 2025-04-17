936d69983e278b4d4e0df4f7c627e1cb8ea3d8ab
Wa_Automata <550578884@qq.com>
2025-04-17 14:52:51 +0800

    公寓管理

--- web/web-admin/src/main/java/com/example/lease/web/admin/service/ApartmentInfoService.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/ApartmentInfoService.java
@@ -12,8 +12,15 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
 * @author cly
 * @description 针对表【apartment_info(公寓信息表)】的数据库操作Service
-* @createDate 2025--03-24 15:48:00
+* @createDate 2025-03-24 15:48:00
 */
 public interface ApartmentInfoService extends IService<ApartmentInfo> {
 
+    void saveOrUpdateApartment(ApartmentSubmitVo apartmentSubmitVo);
+
+    IPage<ApartmentItemVo> pageApartmentItemByQuery(IPage<ApartmentItemVo> page, ApartmentQueryVo queryVo);
+
+    ApartmentDetailVo getApartmentDetailById(Long id);
+
+    void removeApartmentById(Long id);
 }
