c89b8a4427634ea9f3616f2ba65126984eea9795
Wa_Automata <550578884@qq.com>
2025-05-04 21:12:20 +0800

    用户管理,系统管理:后台用户岗位管理,后台用户信息管理

--- web/web-admin/src/main/java/com/example/lease/web/admin/service/SystemUserService.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/SystemUserService.java
@@ -9,8 +9,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
 * @author cly
 * @description 针对表【system_user(员工信息表)】的数据库操作Service
-* @createDate 2025--03-24 15:48:00
+* @createDate 2025-03-24 15:48:00
 */
 public interface SystemUserService extends IService<SystemUser> {
 
+    IPage<SystemUserItemVo> pageSystemUserByQuery(IPage<SystemUser> page, SystemUserQueryVo queryVo);
+
+    SystemUserItemVo getSystemUserById(Long id);
 }
