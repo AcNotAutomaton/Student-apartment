bc387713f4a4b717ca67d0660ad7632bffc62092
Wa_Automata <550578884@qq.com>
2025-05-07 22:41:17 +0800

    登录管理:登陆流程,接口开发

--- web/web-app/src/main/java/com/example/lease/web/app/service/LoginService.java
+++ web/web-app/src/main/java/com/example/lease/web/app/service/LoginService.java
@@ -1,4 +1,13 @@
 package com.example.lease.web.app.service;
 
+import com.example.lease.web.app.vo.user.LoginVo;
+import com.example.lease.web.app.vo.user.UserInfoVo;
+
 public interface LoginService {
+
+    void getSMSCode(String phone);
+
+    String login(LoginVo loginVo);
+
+    UserInfoVo getUserInfoById(Long userId);
 }
--- web/web-app/src/main/java/com/example/lease/web/app/service/SmsService.java
+++ web/web-app/src/main/java/com/example/lease/web/app/service/SmsService.java
@@ -1,4 +1,6 @@
 package com.example.lease.web.app.service;
 
 public interface SmsService {
+    void sendCode(String phone, String verifyCode);
+
 }
